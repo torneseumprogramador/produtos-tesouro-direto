50379528d4bfa539f6446b97490161bc77cb830f
Robson Mendonça <rob.ams@gmail.com>
2020-04-15 11:49:27 -0300

    Adicionado os demais metods no controller.

--- src/controllers/TesouroController.js
+++ src/controllers/TesouroController.js
@@ -3,11 +3,43 @@ const Tesouro = require('../models/Tesouro')
 
 module.exports = {
     async create (req, res) {
-
+        if(req.headers.token === TOKEN){
+            const {nome, ir, pais, preoupos, vencimento} = req.body
+            try {
+              const tesouro =  await Tesouro.create({ nome, ir, pais, preoupos, vencimento});
+              return res.status(201).send(tesouro)
+            } catch (error) {
+              return res.status(401).send(error)
+            }
+          }
+          return res.status(401).json({error: "Acesso não autorizado"})
     },
     async index (req, res) {
-
+        if(req.headers.token === TOKEN){
+            try{
+            const tesouro =  await Tesouro.find({})
+            return res.status(200).send(tesouro)
+            }
+            catch(err){
+              return res.status(401).send(err)
+            }
+          }
+          return res.status(401).json({error: "Acesso não autorizado"})
     },
+
+    async getById (req, res) {
+        const {id} = req.params
+        if(req.headers.token === TOKEN){
+          try {
+            const tesouro =  await Tesouro.findById(id)
+            return res.status(200).send(tesouro)
+          } catch (error) {
+            res.status(401).send(err)
+          }
+        }
+        return res.status(401).json({error: "Acesso não autorizado"})
+      },
+
     async update (req, res) {
         const {nome, taxa, ir, pais, prefixado, vencimento} = req.body
         const {id} = req.params
@@ -19,6 +51,16 @@ module.exports = {
 
     },
     async delete (req, res) {
-
+        const {id} = req.params
+        if(req.headers.token === TOKEN){
+            try{
+              await Tesouro.findByIdAndDelete(id)
+              return res.status(204).send({});
+            }
+            catch(err){
+             return res.status(401).send(err)
+            }
+          }
+          return res.status(401).json({error: "Acesso não autorizado"})
     }
 }
